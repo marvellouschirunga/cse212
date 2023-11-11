0156839f25ad2ab7a4183da07584f7e1a907a524
Marvellous Humphery Chirunga <marvellouschirunga@gmail.com>
2023-11-11 21:46:15 +0200

    Completed Priority queues and TakingTurns

--- week02/code/Priority.cs
+++ week02/code/Priority.cs
@@ -18,23 +18,51 @@ public static class Priority {
         // Test Cases
 
         // Test 1
-        // Scenario: 
-        // Expected Result: 
+        // Scenario: The Enqueue function shall add an item (which contains both data and priority) to the back of the queue.
+        // Expected Result: Adds item to back of the queue
         Console.WriteLine("Test 1");
+        priorityQueue.Enqueue("firstItem", 2);
+        Console.WriteLine(priorityQueue);
 
         // Defect(s) Found: 
 
         Console.WriteLine("---------");
 
         // Test 2
-        // Scenario: 
-        // Expected Result: 
+        // Scenario: The Dequeue function shall remove the item with the highest priority and return its value.
+        // Expected Result: Removes item with highest priority and write to console
         Console.WriteLine("Test 2");
+        priorityQueue.Enqueue("secondItem", 5);
+        Console.WriteLine("Dequeue Result: " + priorityQueue.Dequeue());
+        Console.WriteLine(priorityQueue);
 
-        // Defect(s) Found: 
+        // Defect(s) Found: index was being compared to _queue.Count -1 instead of queue.Count which is the size of the queue
 
         Console.WriteLine("---------");
 
+        // Test 3
+        // Scenario: If there are more than one item with the highest priority, then the item closest to the front of the queue will be removed and its value returned.
+        // Expected Result: Removes the highest priority that's closest to front
+        Console.WriteLine("Test 3");
+        priorityQueue.Enqueue("itemOne", 7);
+        priorityQueue.Enqueue("itemTwo", 7);
+        Console.WriteLine(priorityQueue.Dequeue());
+        Console.WriteLine(priorityQueue);
+
+        // Defect(s) Found: Was not removing the item with the highest priority but only adding it to a variable
+
+        Console.WriteLine("---------");
+
+        // Test 4
+        // Scenario: If the queue is empty, then an error message will be displayed.
+        // Expected Result: Display an error when dequeing an empty queue
+        Console.WriteLine("Test 4");
+        Console.WriteLine("Dequeue Result: " + priorityQueue.Dequeue());
+        Console.WriteLine("Dequeue Result: " + priorityQueue.Dequeue());
+        Console.WriteLine("Dequeue Result: " + priorityQueue.Dequeue());
+        // Defect(s) Found: 
+
+        Console.WriteLine("---------");
         // Add more Test Cases As Needed Below
     }
 }
--- week02/learn/SimpleQueue.cs
+++ week02/learn/SimpleQueue.cs
@@ -20,7 +20,7 @@ public class SimpleQueue {
         queue.Enqueue(100);
         var value = queue.Dequeue();
         Console.WriteLine(value);
-        // Defect(s) Found:
+        // Defect(s) Found: Trying to remove data from index 1 first rather than 0.
 
         Console.WriteLine("------------");
 
@@ -38,7 +38,7 @@ public class SimpleQueue {
         Console.WriteLine(value);
         value = queue.Dequeue();
         Console.WriteLine(value);
-        // Defect(s) Found: 
+        // Defect(s) Found: Program was trying to add values to front of the queue and not back
 
         Console.WriteLine("------------");
 
@@ -54,7 +54,7 @@ public class SimpleQueue {
         catch (IndexOutOfRangeException) {
             Console.WriteLine("I got the exception as expected.");
         }
-        // Defect(s) Found: 
+        // Defect(s) Found: None
     }
 
     private readonly List<int> _queue = new();
@@ -64,7 +64,7 @@ public class SimpleQueue {
     /// </summary>
     /// <param name="value">Integer value to add to the queue</param>
     private void Enqueue(int value) {
-        _queue.Insert(0, value);
+        _queue.Add(value); //Defect 2 - Needs to add new value to the end of the queue not front
     }
 
     /// <summary>
@@ -76,8 +76,8 @@ public class SimpleQueue {
         if (_queue.Count <= 0)
             throw new IndexOutOfRangeException();
 
-        var value = _queue[1];
-        _queue.RemoveAt(1);
+        var value = _queue[0]; //This queue should start at index 0 and not 1
+        _queue.RemoveAt(0);
         return value;
     }
 }
